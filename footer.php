9de999334b0cac6fcdbb9aa57a870f4e673e51b9
mikelinden1 <mike@mikelinden.com>
2016-09-19 14:33:24 -0400

    - added twitter code

--- footer.php
+++ footer.php
@@ -1,3 +1,46 @@
+<?php
+function getLatestTweet() {
+    $latestTweet = getTweets(1);
+    return $latestTweet[0];
+}
+function getTweets($num) {
+    $api_key = urlencode('yaa6xQpwIrIAFhJgsTgpQfqcm'); // Consumer Key (API Key)
+    $api_secret = urlencode('uHCaJ3bQmIMARlbOjsmoMn90siO2le90ltQ9zrZTEQ63dk4oUO'); // Consumer Secret (API Secret)
+    $auth_url = 'https://api.twitter.com/oauth2/token';
+
+    // what we want?
+    $data_username = 'skift'; // username
+    $data_count = $num; // number of tweets
+    $data_url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
+
+    // get api access token
+    $api_credentials = base64_encode($api_key.':'.$api_secret);
+
+    $auth_headers = 'Authorization: Basic '.$api_credentials."\r\n".
+                    'Content-Type: application/x-www-form-urlencoded;charset=UTF-8'."\r\n";
+
+    $auth_context = stream_context_create(
+        array(
+            'http' => array(
+                'header' => $auth_headers,
+                'method' => 'POST',
+                'content'=> http_build_query(array('grant_type' => 'client_credentials', )),
+            )
+        )
+    );
+
+    $auth_response = json_decode(file_get_contents($auth_url, 0, $auth_context), true);
+    $auth_token = $auth_response['access_token'];
+
+    // get tweets
+    $data_context = stream_context_create( array( 'http' => array( 'header' => 'Authorization: Bearer '.$auth_token."\r\n", ) ) );
+
+    $tweets = json_decode(file_get_contents($data_url.'?count='.$data_count.'&screen_name='.urlencode($data_username), 0, $data_context), true);
+
+    return $tweets;
+}   
+?>
+
 <footer id="footer" <?php if(!empty($footerClass)) echo 'class="'.$footerClass.'"'; ?> >
 	<div id="footer-content">
 			<div class="footer-column first">
