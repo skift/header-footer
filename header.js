c3f3c0d6123d1428b2f6da5b684247f3a5208fd1
mikelinden1 <mike@mikelinden.com>
2016-11-01 11:09:52 -0400

    Added airlines ad to header

--- header.js
+++ header.js
@@ -162,6 +162,30 @@ $(function() {
       ignoreContainerHeight: true,
       appendTo: $("#top-banner")
     };
+    
+    var useAirlinesAd = false;
+    
+    if ($("#archive-tag").length) {
+        //tag page eg: airlines
+    
+        var pageName = $("#archive-header-container h1").text();
+        
+        if (pageName === "Airlines") {
+            useAirlinesAd = true;
+        }
+    }
+    
+    if (useAirlinesAd) {
+        console.log("use airlines ad");
+        headerAd = {
+            adClass: 'landscapeAd headerAd',
+            slot: '/22809282/airlines',
+            size: [970,90],
+            ignoreContainerHeight: true,
+            appendTo: $("#top-banner")
+        };
+    }
+        
 
     createAd(headerAd, setHeaderOffset);
   }
