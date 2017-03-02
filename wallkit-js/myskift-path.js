b54336cea33170b7a6e4a647deaa4a04851a5047
mikelinden1 <mike@mikelinden.com>
2017-03-02 16:01:04 -0500

    - further ABSTRACTED the wallkit-js

---
+++ wallkit-js/myskift-path.js
@@ -0,0 +1,16 @@
+var mySkiftPath = "https://my.skift.com/";
+var host = document.location.host;
+
+if (host.indexOf("localhost") > -1) {
+    mySkiftPath = "http://localhost/myskift/";
+}
+
+if (host.indexOf(".wpengine.com") > -1) {
+    mySkiftPath = "https://myskift.wpengine.com/";
+}
+
+if (host.indexOf("myskift.wpengine.com") > -1 || host.indexOf("my.skift.com") > -1) {
+    mySkiftPath = "/";
+}
+
+var mySkiftAjaxPath = mySkiftPath + "ajax/";
--- wallkit-js/wallkit.js
+++ wallkit-js/wallkit.js
@@ -1,56 +1,3 @@
-var hideBannerTimer;
-
-var mySkiftPath = "https://my.skift.com/";
-var host = document.location.host;
-
-if (host.indexOf("localhost") > -1) {
-    mySkiftPath = "http://localhost/myskift/";
-}
-
-if (host.indexOf(".wpengine.com") > -1) {
-    mySkiftPath = "https://myskift.wpengine.com/";
-}
-
-if (host.indexOf("myskift.wpengine.com") > -1 || host.indexOf("my.skift.com") > -1) {
-    mySkiftPath = "/";
-}
-
-var mySkiftAjaxPath = mySkiftPath + "ajax/";
-    
-function setBannerFadeoutTimer(bannerText) {
-    hideBannerTimer = setTimeout(function() {
-        bannerText.fadeOut(function() {
-            $(this).removeClass("show");
-        });
-    }, 6000);
-}
-
-if ($(".error-text").is(":visible")) {
-    setBannerFadeoutTimer($(".error-text"));
-}
-
-function showBannerMessage(message, $form, callback, success) {
-    clearTimeout(hideBannerTimer);
-
-    var bannerText = $form.find(".error-text");
-
-    bannerText.fadeOut(function() {
-        bannerText.text(message).fadeIn(function() {
-            if (typeof callback === "function") {
-                callback();
-            }
-        });
-
-        if (success) {
-            bannerText.addClass("success");
-        } else {
-            bannerText.removeClass("success");
-        }
-
-        setBannerFadeoutTimer(bannerText);
-    });
-}
-
 function getQSParameterByName(name) {
     var url = window.location.href;
     name = name.replace(/[\[\]]/g, "\\$&");
@@ -61,6 +8,8 @@ function getQSParameterByName(name) {
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
+// @codekit-append "myskift-path.js"
+// @codekit-append "banner-messages.js"
 // @codekit-append "cart-functions.js"
 // @codekit-append "mobile-account-manager.js"
 // @codekit-append "login.js"
