7268f4d42c9cbed2c518ae7a43dbda2423b626af
mikelinden1 <mike@mikelinden.com>
2017-08-01 14:39:54 -0400

    - added category ga evebt

--- ads.js
+++ ads.js
@@ -57,6 +57,7 @@ googletag.cmd = googletag.cmd || [];
         // loop through all categories
         for (var i = 0; i < categories.length; i++) {
             thisSector = sectorMapping[categories[i]];
+            ga('send', 'event', 'category', 'page view', categories[i]);
 
             // if we haven't sent a particular sector yet...
             if (typeof thisSector !== 'undefined' && thisSector && sectors.indexOf(thisSector) === -1) {
