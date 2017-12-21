df0bbd7073d74a858f01eb8992eb31ac5ee5fce7
mikelinden1 <mike@mikelinden.com>
2017-12-21 15:03:01 -0500

    switched to single quotes

--- wallkit-js/login.js
+++ wallkit-js/login.js
@@ -1,82 +1,79 @@
 $(function() {
 
-    if ($("#header-sign-in-with-popover").length) {
+    if ($('#header-sign-in-with-popover').length) {
 
         // Header sign in popover
 
         if (!cookiesEnabled()) {
-            addCookieWarning(".login-form .error-text");
+            addCookieWarning('.login-form .error-text');
         }
 
         var clearLoginState = function() {
-            var $form = $("#header-sign-in-with-popover");
-            var bannerText = $form.find(".error-text:not(.stay)");
+            var $form = $('#header-sign-in-with-popover');
+            var bannerText = $form.find('.error-text:not(.stay)');
 
             bannerText.fadeOut(function() {
-                $form.find(".form-group").removeClass("has-error");
+                $form.find('.form-group').removeClass('has-error');
             });
         };
 
-        $("body").keyup(function(e) {
-           if (e.keyCode === 27 && $(".sign-in").hasClass("isOpen")) {
-               $(".sign-in").removeClass("isOpen");
+        $('body').keyup(function(e) {
+           if (e.keyCode === 27 && $('.sign-in').hasClass('isOpen')) {
+               $('.sign-in').removeClass('isOpen');
                clearLoginState();
            }
         });
 
-        $(".sign-in-btn").click(function() {
-            if ($(".shopping-cart").hasClass("isOpen")) {
-                $(".shopping-cart").removeClass("isOpen");
+        $('.sign-in-btn').click(function() {
+            if ($('.shopping-cart').hasClass('isOpen')) {
+                $('.shopping-cart').removeClass('isOpen');
             }
 
-            $(".sign-in").toggleClass("isOpen");
+            $('.sign-in').toggleClass('isOpen');
 
             clearLoginState();
 
-            $("#sign-in-popover input").each(function() {
-                if ($(this).val() !== "") {
-                    $(this).parent().addClass("has-text");
+            $('#sign-in-popover input').each(function() {
+                if ($(this).val() !== '') {
+                    $(this).parent().addClass('has-text');
                 }
             });
         });
 
-        $("#my-account-menu li:first-child a").hover(function() {
-           $("#sign-in-popover").addClass("green-arrow");
+        $('#my-account-menu li:first-child a').hover(function() {
+           $('#sign-in-popover').addClass('green-arrow');
         }, function() {
-           $("#sign-in-popover").removeClass("green-arrow");
+           $('#sign-in-popover').removeClass('green-arrow');
         });
 
-        $("#header-sign-in-with-popover .overlay").click(function() {
-            $(".sign-in, .shopping-cart").removeClass("isOpen");
+        $('#header-sign-in-with-popover .overlay').click(function() {
+            $('.sign-in, .shopping-cart').removeClass('isOpen');
             clearLoginState();
         });
 
-        $(".sign-in").click(function(e) {
+        $('.sign-in').click(function(e) {
            e.stopPropagation();
         });
 
-        $(".has-floating-label").on("change keyup blur input", function() {
-            if ($(this).val() !== "") {
-                $(this).parent().addClass("has-text");
+        $('.has-floating-label').on('change keyup blur input', function() {
+            if ($(this).val() !== '') {
+                $(this).parent().addClass('has-text');
             } else {
-                $(this).parent().removeClass("has-text");
+                $(this).parent().removeClass('has-text');
             }
         });
 
     }
 
-    $(".login-btn").click(function(e) {
-        var $form = $(this).closest(".login-form");
+    $('.login-btn').click(function(e) {
+        var $form = $(this).closest('.login-form');
 
         var goodToGo = true;
 
         $form.find("input").each(function() {
-//                 $(this).parent().removeClass("has-error");
 
             if ($(this).val() === "") {
                 goodToGo = false;
-
-//                     $(this).parent().addClass("has-error");
             }
         });
 
@@ -84,13 +81,13 @@ $(function() {
         var travelDist = 15;
 
         var shake = function(c, times) {
-            c.css("position","relative");
+            c.css('position', 'relative');
 
             c.animate({
-                left: "-"  + travelDist + "px"
+                left: '-'  + travelDist + 'px'
             }, shakeLen, function() {
                 c.animate({
-                    left: travelDist + "px"
+                    left: travelDist + 'px'
                 }, shakeLen, function() {
                     if (times === 0) {
                         c.animate({
@@ -106,7 +103,7 @@ $(function() {
         if (goodToGo) {
             $(this).html('<i class="fa fa-cog fa-spin"></i> Sign In');
 
-            $form.find("input,button").attr("disabled", true);
+            $form.find('input,button').attr('disabled', true);
 
             var loginData = new FormData();
 
@@ -114,8 +111,8 @@ $(function() {
             loginData.append('password', $form.find('.password-field').val());
 
             $.ajax({
-                url: "https://wallkit.skift.com/api/v1/authorization",
-                method: "POST",
+                url: 'https://wallkit.skift.com/api/v1/authorization',
+                method: 'POST',
                 data: loginData,
                 processData: false,
                 contentType: false,
@@ -123,8 +120,8 @@ $(function() {
                     resource: 'K3s0S2lda2dSpDw41ds'
                 },
                 complete: function() {
-                    $form.find("button").html("Sign In");
-                    $form.find("input,button").attr("disabled", false);
+                    $form.find('button').html('Sign In');
+                    $form.find('input,button').attr('disabled', false);
                 },
                 error: function(reason) {
                     var message = reason.responseJSON.error_description ? reason.responseJSON.error_description : 'An uexpected error occured.';
@@ -136,15 +133,15 @@ $(function() {
                         var token = response.token;
                         setCookie('usr', token, 30);
 
-                        showBannerMessage("You are now logged in", $form, function() {
-                            var redirect = $form.find(".login-redirect").val();
+                        showBannerMessage('You are now logged in', $form, function() {
+                            var redirect = $form.find('.login-redirect').val();
 
-                            if (!redirect || redirect === "") {
+                            if (!redirect || redirect === '') {
 
-                                if ($form.hasClass("reload")) {
+                                if ($form.hasClass('reload')) {
                                     var path = location.pathname;
 
-                                    if (path.indexOf("/login") > -1 || path.indexOf("/create-account") > -1) {
+                                    if (path.indexOf('/login') > -1 || path.indexOf('/create-account') > -1) {
                                         location.href = mySkiftPath;
                                     } else {
                                         location.reload();
@@ -164,7 +161,7 @@ $(function() {
             });
 
         } else {
-            showBannerMessage("Please enter both your email address and password.", $form)
+            showBannerMessage('Please enter both your email address and password.', $form)
         }
 
 
@@ -177,8 +174,8 @@ $(function() {
 function setCookie(name, val, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires=" + d.toUTCString();
+    var expires = 'expires=' + d.toUTCString();
 
     var domain = location.hostname === 'localhost' ? 'localhost' : '.' + location.hostname;
-    document.cookie = name + "=" + val + ";" + expires + ";path=/;domain=" + domain;
+    document.cookie = name + '=' + val + ';' + expires + ';path=/;domain=' + domain;
 }
