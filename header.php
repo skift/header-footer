820f0afe1ccd326229db9ffe0497b2f968f2d2f5
mikelinden1 <mike@mikelinden.com>
2018-02-13 14:34:33 -0500

    added instagram to header

--- header.php
+++ header.php
@@ -304,7 +304,7 @@ if (function_exists("is_whitelisted")) {
                     <div class="social-btn" title="Facebook"><a href="https://www.facebook.com/Skiftnews/" target="_blank"><i class="fa fa-facebook"></i></a></div>
                     <div class="social-btn" title="Twitter"><a href="https://twitter.com/skift" target="_blank"><i class="fa fa-twitter"></i></a></div>
                     <div class="social-btn" title="LinkedIn"><a href="https://www.linkedin.com/company/2641998" target="_blank"><i class="fa fa-linkedin"></i></a></div>
-    <!--                 <div class="header-social-btn email" title="Email"><a href="javascript:"><i class="fa fa-envelope"></i></a></div> -->
+                    <div class="social-btn instagram" title="Instagram"><a href="https://instagram.com/skiftnews" target="_blank"><i class="fa fa-instagram"></i></a></div>
                 </div>
             <?php } ?>
 
