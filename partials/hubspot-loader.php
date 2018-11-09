6e5c017fbd1e761728fab64ff1fbcb8ff2f954f1
mikelinden1 <mike@mikelinden.com>
2018-11-09 12:57:06 -0500

    move format function and run everytime there's a change so progressive fields format

--- partials/hubspot-loader.php
+++ partials/hubspot-loader.php
@@ -13,7 +13,80 @@
             form.inlineMessage = form.inlineMessage || 'Thank you!'
         }
 
-        form.onBeforeFormInit= function(ctx) {
+        var formatForm = function(form) {
+            console.log('format');
+
+            form.find(".hs-form-field").addClass("form-group");
+            form.find(".hs-form-checkbox, .hs-form-booleancheckbox").addClass("checkbox-control");
+            form.find(".hs-form-radio").addClass("radio-control");
+            form.find("label").addClass("control-label");
+            form.find(".hs-input").addClass("form-control");
+
+            form.find(".inputs-list").css({
+                listStyle: 'none',
+                padding: 0,
+                margin: 0,
+                textAlign: 'left'
+            });
+
+            form.find(".hs-field-desc").css({
+                fontSize: '12px',
+                paddingLeft: '12px',
+                borderBottom: 'none',
+                marginBottom: '10px'
+            });
+
+            form.find("li.hs-form-radio span, li.hs-form-booleancheckbox span, .hs-form-checkbox-display span").addClass("control-label");
+            form.find("li.hs-form-radio label, li.hs-form-booleancheckbox label, .hs-form-checkbox-display").removeClass("control-label");
+
+            form.find(".hs-form-checkbox-display").addClass("checkbox-control").find("span").addClass("control-label");
+
+            form.find(".hs-form-checkbox:not(li)").each(function() {
+                $(this).find(".form-control").prependTo($(this));
+            });
+
+            if (!form.hasClass("noPlaceholders")) {
+                form.find(".hs-input").each(function() {
+                    if ($(this).attr("placeholder") === "") {
+                        var fieldName = $(this).closest(".hs-form-field").find("label span:first").text();
+                        $(this).attr("placeholder",fieldName);
+                    }
+                });
+            }
+
+            form.find(".hs-input").blur(function() {
+                var $this = $(this);
+                setTimeout(function() {
+                    if ($this.hasClass("error")) {
+                        $this.attr("class","form-control error").closest(".hs-form-field").addClass("has-error").find(".hs-error-msgs").show();
+                    } else {
+                        $this.closest(".hs-form-field").removeClass("has-error");
+                    }
+                },100);
+            }).keyup(function() {
+                var $this = $(this);
+                if (!$this.hasClass("error")) {
+                    $this.closest(".hs-form-field").removeClass("has-error").find(".hs-error-msgs").hide();
+                }
+            });
+
+            form.find(".hs-fieldtype-select").change(function() {
+                form.find(".hs-form-field").addClass("form-group");
+                form.find("label").addClass("control-label");
+                form.find(".hs-input").addClass("form-control");
+            });
+
+            if (form.hasClass("hideLabels")) {
+                form.find("label").hide();
+            }
+
+            if (typeof prevOnReady === "function") {
+                prevOnReady(form);
+            }
+
+        };
+
+        form.onBeforeFormInit = function(ctx) {
             var prevOnReady = ctx.onFormReady;
             var formContainer = ctx.formContainer;
 
@@ -21,73 +94,12 @@
                 if (typeof formContainer !== "undefined" && formContainer) {
                     form.parent().appendTo(formContainer);
                 }
-
-                form.find(".hs-form-field").addClass("form-group");
-                form.find(".hs-form-checkbox, .hs-form-booleancheckbox").addClass("checkbox-control");
-                form.find(".hs-form-radio").addClass("radio-control");
-                form.find("label").addClass("control-label");
-                form.find(".hs-input").addClass("form-control");
-
-                form.find(".inputs-list").css({
-                    listStyle: 'none',
-                    padding: 0,
-                    margin: 0
+            
+                form.find('input[type=checkbox], select').on('click change', function() {
+                    formatForm(form);
                 });
 
-                form.find(".hs-field-desc").css({
-                    fontSize: '12px',
-                    paddingLeft: '12px',
-                    borderBottom: 'none',
-                    marginBottom: '10px'
-                });
-
-                form.find("li.hs-form-radio span, li.hs-form-booleancheckbox span, .hs-form-checkbox-display span").addClass("control-label");
-                form.find("li.hs-form-radio label, li.hs-form-booleancheckbox label, .hs-form-checkbox-display").removeClass("control-label");
-
-
-                form.find(".hs-form-checkbox:not(li)").each(function() {
-                    $(this).find(".form-control").prependTo($(this));
-                });
-
-                if (!form.hasClass("noPlaceholders")) {
-                    form.find(".hs-input").each(function() {
-                        if ($(this).attr("placeholder") === "") {
-                            var fieldName = $(this).closest(".hs-form-field").find("label span:first").text();
-                            $(this).attr("placeholder",fieldName);
-                        }
-                    });
-                }
-
-                form.find(".hs-input").blur(function() {
-                    var $this = $(this);
-                    setTimeout(function() {
-                        if ($this.hasClass("error")) {
-                            $this.attr("class","form-control error").closest(".hs-form-field").addClass("has-error").find(".hs-error-msgs").show();
-                        } else {
-                            $this.closest(".hs-form-field").removeClass("has-error");
-                        }
-                    },100);
-                }).keyup(function() {
-                    var $this = $(this);
-                    if (!$this.hasClass("error")) {
-                        $this.closest(".hs-form-field").removeClass("has-error").find(".hs-error-msgs").hide();
-                    }
-                });
-
-                form.find(".hs-fieldtype-select").change(function() {
-                    form.find(".hs-form-field").addClass("form-group");
-                    form.find("label").addClass("control-label");
-                    form.find(".hs-input").addClass("form-control");
-                });
-
-                if (form.hasClass("hideLabels")) {
-                    form.find("label").hide();
-                }
-
-                if (typeof prevOnReady === "function") {
-                    prevOnReady(form);
-                }
-
+                formatForm(form);
             };
 
             return ctx;
