29f5f8ad54acb863656297dbbc613c8381ff4e1a
mikelinden1 <mike@mikelinden.com>
2018-12-12 09:52:53 -0500

    added nofollow to social links

--- partials/header-social.php
+++ partials/header-social.php
@@ -1,6 +1,6 @@
 <div id="header-social">
-    <div class="social-btn" title="Facebook"><a href="https://www.facebook.com/Skiftnews/" target="_blank" rel="noopener noreferrer"><i class="fa fa-facebook"></i></a></div>
-    <div class="social-btn" title="Twitter"><a href="https://twitter.com/skift" target="_blank" rel="noopener noreferrer"><i class="fa fa-twitter"></i></a></div>
-    <div class="social-btn" title="LinkedIn"><a href="https://www.linkedin.com/company/2641998" target="_blank" rel="noopener noreferrer"><i class="fa fa-linkedin"></i></a></div>
-    <div class="social-btn instagram" title="Instagram"><a href="https://instagram.com/skiftnews" target="_blank" rel="noopener noreferrer"><i class="fa fa-instagram"></i></a></div>
+    <div class="social-btn" title="Facebook"><a href="https://www.facebook.com/Skiftnews/" target="_blank" rel="nofollow noopener noreferrer"><i class="fa fa-facebook"></i></a></div>
+    <div class="social-btn" title="Twitter"><a href="https://twitter.com/skift" target="_blank" rel="nofollow noopener noreferrer"><i class="fa fa-twitter"></i></a></div>
+    <div class="social-btn" title="LinkedIn"><a href="https://www.linkedin.com/company/2641998" target="_blank" rel="nofollow noopener noreferrer"><i class="fa fa-linkedin"></i></a></div>
+    <div class="social-btn instagram" title="Instagram"><a href="https://instagram.com/skiftnews" target="_blank" rel="nofollow noopener noreferrer"><i class="fa fa-instagram"></i></a></div>
 </div>
