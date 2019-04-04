1d09d5c198c3bc119b103732ff8c0bf10df18736
mikelinden1 <mike@mikelinden.com>
2019-04-04 16:48:30 -0400

    changed mobile newsletter link

--- partials/mobile.php
+++ partials/mobile.php
@@ -33,7 +33,7 @@
             <li class="menu-item"><a href="<?php echo $url_paths["main"]; ?>/news/">News</a></li>
             <li class="menu-item"><a href="<?php echo $url_paths["trends"]; ?>">Research</a></li>
             <li class="menu-item"><a href="<?php echo $url_paths["forum"]; ?>">Conferences</a></li>
-            <li class="menu-item"><a href="<?php echo $url_paths["main"]; ?>/newsletters">Newsletters</a></li>
+            <li class="menu-item"><a href="<?php echo $url_paths["main"]; ?>/daily">Newsletters</a></li>
             <li class="menu-item"><a href="<?php echo $url_paths["main"]; ?>/advertising">Advertising</a></li>
             <li class="menu-item"><a href="<?php echo $url_paths["table"]; ?>">Skift Table</a></li>
             <?php if ($show_wellness_logo) { ?>
