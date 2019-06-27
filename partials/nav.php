42b25521e1099ad1e232e823ad6475255a25f85b
Rachel Bronstein <rnbronstein@gmail.com>
2019-06-27 18:36:37 -0400

    Add filter for nav menu HTML

--- partials/nav.php
+++ partials/nav.php
@@ -2,11 +2,16 @@
     <ul id="navigation-menu" style="max-width: <?php echo $primary_nav_max_width; ?>;">
         <?php
         do_action('sk_header_before_nav', $primary_nav_name);
-        wp_nav_menu(array(
+
+        $menu_html = wp_nav_menu(array(
             'theme_location' => $primary_nav_name,
             'container' => false,
-            'items_wrap' => '%3$s'
+            'items_wrap' => '%3$s',
+            'echo' => false
         ));
+
+        echo apply_filters('sk_header_primary_nav', $menu_html, $primary_nav_name);
+        
         do_action('sk_header_after_nav', $primary_nav_name);
         ?>
     </ul>
