927e279a1c052cd0b5b752ca6919d86419044af0
rnbronstein <rnbronstein@gmail.com>
2019-08-04 19:59:25 -0400

    Update user functions

--- partials/wallkit.php
+++ partials/wallkit.php
@@ -3,11 +3,43 @@
     <?php
     if (!$signed_in && !$white_listed) {
         ?>
-        <div class="sign-in"><a href="<?php mysk_the_login_link();?>" class="sign-in-btn">Sign In</a></div>
+        <div class="sign-in"><a href="<?php mysk_the_login_link(); ?>" class="sign-in-btn">Sign In</a></div>
         <?php
     } else if ($white_listed) {
         ?>
         <div class="sign-in"><?php echo "Welcome $white_listed!"; ?></div>
+        <?php
+    } else {
+        ?>
+        <div class="sign-in hasPopover">
+            <a href="javascript:" class="sign-in-btn my-account-btn">
+                <span class="fa-stack fa-lg">
+                    <i class="fa fa-circle-thin fa-stack-2x"></i>
+                    <i class="fa fa-user fa-stack-1x"></i>
+                </span>
+            </a>
+
+            <div id="sign-in-popover" class="popover">
+                <ul id="my-account-menu">
+                    <li><a href="<?php mysk_the_dashboard_link(); ?>">Account</a></li>
+                    <li><a href="<?php mysk_the_logout_link();  ?>" class="logout-btn">Sign Out</a></li>
+                </ul>
+
+                <?php
+                global $mysk_current_user;
+                if ($mysk_current_user && !empty($mysk_current_user->name)) {
+                    ?>
+                    <p>Welcome, <?php echo $mysk_current_user->name; ?>!</p>
+                    <?php
+                } else {
+                    ?>
+                    <p>Welcome!</p>
+                    <?php
+                }
+                ?>
+            </div>
+        </div>
+        <div class="overlay"></div>
         <?php
     }
     ?>
