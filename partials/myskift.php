c80b8643c70a3f752d2449279d08716b780aebbd
mikelinden1 <mike@mikelinden.com>
2021-03-12 12:21:58 -0500

    remove account button if now signed in

--- partials/myskift.php
+++ partials/myskift.php
@@ -17,7 +17,14 @@
 
             <div class="account-menu-popover">
                 <ul>
-                    <li><a href="<?php if (function_exists('mysk_get_dashboard_link')) echo mysk_get_dashboard_link(); ?>" rel="nofollow">Account</a></li>
+                    <?php
+                    $logged_in = function_exists('mysk_current_reader_is_logged_in') ? mysk_current_reader_is_logged_in() : false;
+                    if ($logged_in) {
+                        ?>
+                        <li><a href="<?php if (function_exists('mysk_get_dashboard_link')) echo mysk_get_dashboard_link(); ?>" rel="nofollow">Account</a></li>
+                        <?php
+                    }
+                    ?>
                     <li><a href="<?php echo $logout_link; ?>" class="sign-out-btn trigger-sign-out" rel="nofollow">Sign Out</a></li>
                 </ul>
 
