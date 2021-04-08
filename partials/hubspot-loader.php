e85f2e018f373b182c801e99deb31b2dc5aa2ab2
mikelinden1 <mike@mikelinden.com>
2021-04-08 13:38:23 -0400

    copy hubspot form fix over

--- partials/hubspot-loader.php
+++ partials/hubspot-loader.php
@@ -111,7 +111,7 @@
             document.head.appendChild(hsScripts);
         }
 
-        if (typeof hbspt !== "undefined" && hbspt) {
+        if (window.hbspt && window.hbspt.forms && typeof window.hbspt.forms.create === 'function') {
 
             //hubspot library is loaded, load the forms
             for (var i = queuedForms.length-1; i >= 0; i--) {
